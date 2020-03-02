7a1890f7966b8bafee60c2890725e256319db750
amir hossein Rezayy <ah.rezayy@chmail.ir>
2020-03-02 18:42:27 +0330

    2

---
+++ precache-manifest.928f22b03fdcd7be84ce1cdd385b8730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a1c08cbbda3fa4b015cb97998eec30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d89c144840cd465ea504",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d89c144840cd465ea504",
+    "url": "/static/js/2.26c319fa.chunk.js"
+  },
+  {
+    "revision": "4ea0dd34ee1ea33eb76a34b399b79e7e",
+    "url": "/static/js/2.26c319fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0187f5ccdf00c22c5d1",
+    "url": "/static/js/main.79f12283.chunk.js"
+  },
+  {
+    "revision": "090c8dceb2a15ae91adb",
+    "url": "/static/js/runtime-main.45936b65.js"
+  }
+]);
